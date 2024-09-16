ad20a4bc7ff767f9bc57f0791a637584ddcb8245
bucky1202 <bakyegenov@gmail.com>
2024-09-16 19:46:53 +0500

    fake the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,21 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name,
+            'balance' => $this->faker->randomFloat(2, 0, 1000), // Random balance between 0 and 1000
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
